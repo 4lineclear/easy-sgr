f026bceb262a846b92c289c8fa1affea2fb2c3fb
4lineclear <tamimhakimi21@gmail.com>
2023-07-06 15:40:58 -0400

    Fixed README

--- src/lib.rs
+++ src/lib.rs
@@ -90,8 +90,8 @@
 //! use easy_sgr::{ColorKind, Clear::Reset, SGRString, StyleKind};
 //!
 //! let mut text = SGRString::from("This should be italic & red!");
-//! text.italic = StyleKind::Place;
-//! text.foreground = ColorKind::Red;
+//! text.graphics.italic = StyleKind::Place;
+//! text.graphics.foreground = ColorKind::Red;
 //!
 //! println!("{text}")
 //! ```

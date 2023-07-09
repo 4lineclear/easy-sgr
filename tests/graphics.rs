aac1bd52fa6468c6be80646040f38c62ab2165b3
4lineclear <tamimhakimi21@gmail.com>
2023-07-09 00:45:12 -0400

    Expanded graphics tests

--- tests/graphics.rs
+++ tests/graphics.rs
@@ -107,9 +107,61 @@ fn colors() {
 fn easy_sgr() {
     assert_eq!("test", "test".to_sgr().to_string());
     assert_eq!("test", "".text("test").to_string());
-    assert_eq!("\x1b[3mtest", "test".style(Italic).to_string());
-    assert_eq!("\x1b[31mtest", "test".color(RedFg).to_string());
     assert_eq!("\x1b[100mtest", "test".custom(100).to_string());
     assert_eq!("\x1b[100mtest", "test".custom_place(100).to_string());
     assert_eq!("test\x1b[100m", "test".custom_clean(100).to_string());
+
+    assert_eq!("\x1b[31;3m", Italic.color(RedFg).to_string());
+    assert_eq!("\x1b[31;3m", RedFg.style(Italic).to_string());
+}
+
+#[test]
+fn easy_sgr_style() {
+    for (correct, style) in [
+        ("\x1b[0m", Reset),
+        ("\x1b[1m", Bold),
+        ("\x1b[2m", Dim),
+        ("\x1b[3m", Italic),
+        ("\x1b[4m", Underline),
+        ("\x1b[5m", Blinking),
+        ("\x1b[7m", Inverse),
+        ("\x1b[8m", Hidden),
+        ("\x1b[9m", Strikethrough),
+        ("\x1b[22m", NotBold),
+        ("\x1b[22m", NotDim),
+        ("\x1b[23m", NotItalic),
+        ("\x1b[24m", NotUnderline),
+        ("\x1b[25m", NotBlinking),
+        ("\x1b[27m", NotInverse),
+        ("\x1b[28m", NotHidden),
+        ("\x1b[29m", NotStrikethrough),
+    ] {
+        assert_eq!(correct, "".style(style).to_string());
+    }
+}
+
+#[test]
+fn easy_sgr_color() {
+    for (correct, color) in [
+        ("\x1b[30m", BlackFg),
+        ("\x1b[31m", RedFg),
+        ("\x1b[32m", GreenFg),
+        ("\x1b[33m", YellowFg),
+        ("\x1b[34m", BlueFg),
+        ("\x1b[35m", MagentaFg),
+        ("\x1b[36m", CyanFg),
+        ("\x1b[37m", WhiteFg),
+        ("\x1b[39m", DefaultFg),
+        ("\x1b[40m", BlackBg),
+        ("\x1b[41m", RedBg),
+        ("\x1b[42m", GreenBg),
+        ("\x1b[43m", YellowBg),
+        ("\x1b[44m", BlueBg),
+        ("\x1b[45m", MagentaBg),
+        ("\x1b[46m", CyanBg),
+        ("\x1b[47m", WhiteBg),
+        ("\x1b[49m", DefaultBg),
+    ] {
+        assert_eq!(correct, "".color(color).to_string())
+    }
 }

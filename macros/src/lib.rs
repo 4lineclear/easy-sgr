1ca646e6f61fbc8962f634e508ee0ed4711977a8
4lineclear <tamimhakimi21@gmail.com>
2023-07-14 18:55:13 -0400

    Added method to remove escapes

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,41 +1,94 @@
-use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
+// use std::num::ParseIntError;
 
-enum ParseError {
-    MissingStrLit,
-}
+use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
 
 #[proc_macro]
 pub fn replace_sgr(input: TokenStream) -> TokenStream {
     match replace_sgr_impl(input) {
-        Ok(tokens) => tokens,
-        Err(e) => [
-            TokenTree::Ident(Ident::new("compile_error", Span::mixed_site())),
-            TokenTree::Punct(Punct::new('!', Spacing::Alone)),
-            TokenTree::Group(Group::new(
-                Delimiter::Parenthesis,
-                [TokenTree::Literal(Literal::string(match e {
-                    ParseError::MissingStrLit => "Missing String literal!",
-                }))]
-                .into_iter()
-                .collect(),
-            )),
-        ]
-        .into_iter()
-        .collect(),
+        Some(tokens) => tokens,
+        None => err(),
     }
 }
-
-fn replace_sgr_impl(input: TokenStream) -> Result<TokenStream, ParseError> {
-    Ok([TokenTree::Literal(Literal::string(
-        &input
+fn err() -> TokenStream {
+    [
+        TokenTree::Ident(Ident::new("compile_error", Span::mixed_site())),
+        TokenTree::Punct(Punct::new('!', Spacing::Alone)),
+        TokenTree::Group(Group::new(
+            Delimiter::Parenthesis,
+            [TokenTree::Literal(Literal::string(
+                "requires a string literal\ncannot be raw and/or byte string",
+            ))]
             .into_iter()
-            .next()
-            .map(|x| match x {
-                proc_macro::TokenTree::Literal(literal) => Ok(literal.to_string()),
-                _ => Err(ParseError::MissingStrLit),
-            })
-            .ok_or(ParseError::MissingStrLit)??,
-    ))]
+            .collect(),
+        )),
+    ]
     .into_iter()
-    .collect())
+    .collect()
+}
+fn replace_sgr_impl(input: TokenStream) -> Option<TokenStream> {
+    Some(
+        [TokenTree::Literal(Literal::string(&remove_escapes(
+            input
+                .into_iter()
+                .next()
+                .map(|x| match x {
+                    proc_macro::TokenTree::Literal(literal) => Some(literal.to_string()),
+                    _ => None,
+                })??
+                .strip_suffix('"')?
+                .strip_prefix('"')?,
+        )?))]
+        .into_iter()
+        .collect(),
+    )
+}
+fn remove_escapes(src: &str) -> Option<String> {
+    let mut s = String::with_capacity(src.len());
+    let mut chars = src.chars();
+    while let Some(c) = chars.next() {
+        if let '\\' = c {
+            if let Some(c_next) = chars.next() {
+                match c_next {
+                    //quote escapes
+                    '\'' => s.push('\''),
+                    '"' => s.push('"'),
+                    //ascii escapes
+                    'x' => s.push(parse_7bit(&mut chars)?),
+                    'n' => s.push('\n'),
+                    'r' => s.push('\r'),
+                    't' => s.push('\t'),
+                    '\\' => s.push('\\'),
+                    '\0' => s.push('\0'),
+                    //unicode escape
+                    'u' => s.push(parse_24bit(&mut chars)?),
+                    //whitespace ignore
+                    '\n' => {
+                        for c in chars.by_ref() {
+                            let (' ' | '\n' | '\r' | '\t') = c else {
+                                s.push(c);
+                                break;
+                            };
+                        }
+                    }
+                    _ => (),
+                }
+            }
+        } else {
+            s.push(c)
+        }
+    }
+    Some(s)
+}
+fn parse_7bit(chars: &mut impl Iterator<Item = char>) -> Option<char> {
+    let mut src = String::with_capacity(2);
+    src.push(chars.next()?);
+    src.push(chars.next()?);
+
+    char::from_u32(u32::from_str_radix(&src, 16).ok()?)
+}
+fn parse_24bit(chars: &mut impl Iterator<Item = char>) -> Option<char> {
+    chars.next()?;
+    let src: String = chars.take_while(|&c| c != '}').collect();
+
+    char::from_u32(u32::from_str_radix(&src, 16).ok()?)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use easy_sgr_macros::replace_sgr;
 
 fn main() {
-    let test = replace_sgr!("Test");
-    println!("{test}");
+    println!(
+        "'\'\"\x1b[0mTest\x7f\u{24} \
+    what is this\r\t\0\n \
+    "
+    );
+    const TEST: &str = replace_sgr!(
+        "'\'\"\x1b[0mTest\x7f\u{24} \
+    what is this\r\t\0\n \
+    "
+    );
+    println!("{TEST}");
 }

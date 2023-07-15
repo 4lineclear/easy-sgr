b072ec32736b59e31ebe2a227df217340842363e
4lineclear <tamimhakimi21@gmail.com>
2023-07-15 15:27:49 -0400

    Moved forms to form.rs

---
+++ macros/src/form.rs
@@ -0,0 +1,75 @@
+#[derive(Clone)]
+pub(crate) struct Transform<I, F> {
+    iter: I,
+    f: F,
+}
+impl<I, F> Iterator for Transform<I, F>
+where
+    I: Iterator,
+    F: FnMut(&mut I) -> Option<I::Item>,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (self.f)(&mut self.iter)
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+pub(crate) trait ToTransform<I, F>
+where
+    I: Iterator,
+    F: FnMut(&mut I) -> Option<I::Item>,
+{
+    fn transform(self, f: F) -> Transform<I, F>;
+}
+impl<I, F> ToTransform<I, F> for I
+where
+    I: Iterator,
+    F: FnMut(&mut I) -> Option<I::Item>,
+{
+    fn transform(self, f: F) -> Transform<I, F> {
+        Transform { iter: self, f }
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct Mapform<I, F> {
+    iter: I,
+    f: F,
+}
+impl<I, F, N> Iterator for Mapform<I, F>
+where
+    I: Iterator,
+    F: FnMut(&mut I) -> Option<N>,
+{
+    type Item = N;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (self.f)(&mut self.iter)
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+pub(crate) trait ToMapform<I, F, N>
+where
+    F: FnMut(&mut I) -> N,
+{
+    fn mapform(self, f: F) -> Mapform<I, F>;
+}
+impl<I, F, N> ToMapform<I, F, N> for I
+where
+    F: FnMut(&mut I) -> N,
+{
+    fn mapform(self, f: F) -> Mapform<I, F> {
+        Mapform { iter: self, f }
+    }
+}
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,6 +2,7 @@ use parse::parse_string;
 use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
 use sgr::sgrargs;
 
+mod form;
 mod parse;
 mod sgr;
 
@@ -61,80 +62,4 @@ fn sgr_args(source: &TokenTree) -> Option<TokenStream> {
         .into_iter()
         .collect(),
     )
-}
-
-#[derive(Clone)]
-struct Transform<I, F> {
-    iter: I,
-    f: F,
-}
-impl<I, F> Iterator for Transform<I, F>
-where
-    I: Iterator,
-    F: FnMut(&mut I) -> Option<I::Item>,
-{
-    type Item = I::Item;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        (self.f)(&mut self.iter)
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.iter.size_hint()
-    }
-}
-
-trait ToTransform<I, F>
-where
-    I: Iterator,
-    F: FnMut(&mut I) -> Option<I::Item>,
-{
-    fn transform(self, f: F) -> Transform<I, F>;
-}
-impl<I, F> ToTransform<I, F> for I
-where
-    I: Iterator,
-    F: FnMut(&mut I) -> Option<I::Item>,
-{
-    fn transform(self, f: F) -> Transform<I, F> {
-        Transform { iter: self, f }
-    }
-}
-
-#[derive(Clone)]
-struct Mapform<I, F> {
-    iter: I,
-    f: F,
-}
-impl<I, F, N> Iterator for Mapform<I, F>
-where
-    I: Iterator,
-    F: FnMut(&mut I) -> Option<N>,
-{
-    type Item = N;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        (self.f)(&mut self.iter)
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.iter.size_hint()
-    }
-}
-
-trait ToMapform<I, F, N>
-where
-    F: FnMut(&mut I) -> N,
-{
-    fn mapform(self, f: F) -> Mapform<I, F>;
-}
-impl<I, F, N> ToMapform<I, F, N> for I
-where
-    F: FnMut(&mut I) -> N,
-{
-    fn mapform(self, f: F) -> Mapform<I, F> {
-        Mapform { iter: self, f }
-    }
 }

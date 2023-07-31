5d3a08423007d5a334e102ca6d979b6d7e94b5da
4lineclear <tamimhakimi21@gmail.com>
2023-07-31 17:12:40 -0400

    Tests now run using a macro for readability

--- tests/macro.rs
+++ tests/macro.rs
@@ -1,69 +1,83 @@
+use easy_sgr::sgr;
+
+macro_rules! sgr_tests {
+    ($($input:tt = $result:literal),*) => {
+        $(
+            assert_eq!($result, sgr!($input));
+        )*
+    };
+}
+
 #[test]
 fn styles() {
-    assert_eq!("\x1b[0m", easy_sgr::format!("{+Reset}"));
-    assert_eq!("\x1b[1m", easy_sgr::format!("{+Bold}"));
-    assert_eq!("\x1b[2m", easy_sgr::format!("{+Dim}"));
-    assert_eq!("\x1b[3m", easy_sgr::format!("{+Italic}"));
-    assert_eq!("\x1b[4m", easy_sgr::format!("{+Underline}"));
-    assert_eq!("\x1b[5m", easy_sgr::format!("{+Blink}"));
-    assert_eq!("\x1b[7m", easy_sgr::format!("{+Inverse}"));
-    assert_eq!("\x1b[8m", easy_sgr::format!("{+Hide}"));
-    assert_eq!("\x1b[9m", easy_sgr::format!("{+Strike}"));
-    assert_eq!("\x1b[22m", easy_sgr::format!("{-Bold}"));
-    assert_eq!("\x1b[22m", easy_sgr::format!("{-Dim}"));
-    assert_eq!("\x1b[23m", easy_sgr::format!("{-Italic}"));
-    assert_eq!("\x1b[24m", easy_sgr::format!("{-Underline}"));
-    assert_eq!("\x1b[25m", easy_sgr::format!("{-Blink}"));
-    assert_eq!("\x1b[27m", easy_sgr::format!("{-Inverse}"));
-    assert_eq!("\x1b[28m", easy_sgr::format!("{-Hide}"));
-    assert_eq!("\x1b[29m", easy_sgr::format!("{-Strike}"));
+    sgr_tests!(
+        "{+Reset}" = "\x1b[0m",
+        "{+Bold}" = "\x1b[1m",
+        "{+Dim}" = "\x1b[2m",
+        "{+Italic}" = "\x1b[3m",
+        "{+Underline}" = "\x1b[4m",
+        "{+Blink}" = "\x1b[5m",
+        "{+Inverse}" = "\x1b[7m",
+        "{+Hide}" = "\x1b[8m",
+        "{+Strike}" = "\x1b[9m",
+        "{-Bold}" = "\x1b[22m",
+        "{-Dim}" = "\x1b[22m",
+        "{-Italic}" = "\x1b[23m",
+        "{-Underline}" = "\x1b[24m",
+        "{-Blink}" = "\x1b[25m",
+        "{-Inverse}" = "\x1b[27m",
+        "{-Hide}" = "\x1b[28m",
+        "{-Strike}" = "\x1b[29m"
+    );
 }
 #[test]
 fn standard_colors() {
-    assert_eq!("\x1b[30m", easy_sgr::format!("{#BlackFg}"));
-    assert_eq!("\x1b[31m", easy_sgr::format!("{#RedFg}"));
-    assert_eq!("\x1b[32m", easy_sgr::format!("{#GreenFg}"));
-    assert_eq!("\x1b[33m", easy_sgr::format!("{#YellowFg}"));
-    assert_eq!("\x1b[34m", easy_sgr::format!("{#BlueFg}"));
-    assert_eq!("\x1b[35m", easy_sgr::format!("{#MagentaFg}"));
-    assert_eq!("\x1b[36m", easy_sgr::format!("{#CyanFg}"));
-    assert_eq!("\x1b[37m", easy_sgr::format!("{#WhiteFg}"));
-    assert_eq!("\x1b[39m", easy_sgr::format!("{#DefaultFg}"));
-    assert_eq!("\x1b[40m", easy_sgr::format!("{#BlackBg}"));
-    assert_eq!("\x1b[41m", easy_sgr::format!("{#RedBg}"));
-    assert_eq!("\x1b[42m", easy_sgr::format!("{#GreenBg}"));
-    assert_eq!("\x1b[43m", easy_sgr::format!("{#YellowBg}"));
-    assert_eq!("\x1b[44m", easy_sgr::format!("{#BlueBg}"));
-    assert_eq!("\x1b[45m", easy_sgr::format!("{#MagentaBg}"));
-    assert_eq!("\x1b[46m", easy_sgr::format!("{#CyanBg}"));
-    assert_eq!("\x1b[47m", easy_sgr::format!("{#WhiteBg}"));
-    assert_eq!("\x1b[49m", easy_sgr::format!("{#DefaultBg}"));
+    sgr_tests!(
+        "{#BlackFg}" = "\x1b[30m",
+        "{#RedFg}" = "\x1b[31m",
+        "{#GreenFg}" = "\x1b[32m",
+        "{#YellowFg}" = "\x1b[33m",
+        "{#BlueFg}" = "\x1b[34m",
+        "{#MagentaFg}" = "\x1b[35m",
+        "{#CyanFg}" = "\x1b[36m",
+        "{#WhiteFg}" = "\x1b[37m",
+        "{#DefaultFg}" = "\x1b[39m",
+        "{#BlackBg}" = "\x1b[40m",
+        "{#RedBg}" = "\x1b[41m",
+        "{#GreenBg}" = "\x1b[42m",
+        "{#YellowBg}" = "\x1b[43m",
+        "{#BlueBg}" = "\x1b[44m",
+        "{#MagentaBg}" = "\x1b[45m",
+        "{#CyanBg}" = "\x1b[46m",
+        "{#WhiteBg}" = "\x1b[47m",
+        "{#DefaultBg}" = "\x1b[49m"
+    );
 }
 
 #[test]
 fn byte_color() {
-    assert_eq!(format!("\x1b[38;5;0m"), easy_sgr::format!("{#f(0)}"));
-    assert_eq!(format!("\x1b[48;5;0m"), easy_sgr::format!("{#b(0)}"));
-    assert_eq!(format!("\x1b[38;5;255m"), easy_sgr::format!("{#f(255)}"));
-    assert_eq!(format!("\x1b[48;5;255m"), easy_sgr::format!("{#b(255)}"));
+    sgr_tests!(
+        "{#f(0)}" = "\x1b[38;5;0m",
+        "{#b(0)}" = "\x1b[48;5;0m",
+        "{#f(255)}" = "\x1b[38;5;255m",
+        "{#b(255)}" = "\x1b[48;5;255m",
+        "{#f[00]}" = "\x1b[38;5;0m",
+        "{#b[00]}" = "\x1b[48;5;0m",
+        "{#f[ff]}" = "\x1b[38;5;255m",
+        "{#b[ff]}" = "\x1b[48;5;255m"
+    );
 }
 
 #[test]
 fn rgb_color() {
-    assert_eq!(
-        format!("\x1b[38;2;0;0;0m"),
-        easy_sgr::format!("{#f(0,0,0)}")
-    );
-    assert_eq!(
-        format!("\x1b[48;2;0;0;0m"),
-        easy_sgr::format!("{#b(0,0,0)}")
-    );
-    assert_eq!(
-        format!("\x1b[38;2;255;255;255m"),
-        easy_sgr::format!("{#f(255,255,255)}")
-    );
-    assert_eq!(
-        format!("\x1b[48;2;255;255;255m"),
-        easy_sgr::format!("{#b(255,255,255)}")
+    sgr_tests!(
+        "{#f(0,0,0)}" = "\x1b[38;2;0;0;0m",
+        "{#b(0,0,0)}" = "\x1b[48;2;0;0;0m",
+        "{#f(255,255,255)}" = "\x1b[38;2;255;255;255m",
+        "{#b(255,255,255)}" = "\x1b[48;2;255;255;255m",
+        "{#f[000000]}" = "\x1b[38;2;0;0;0m",
+        "{#b[000000]}" = "\x1b[48;2;0;0;0m",
+        "{#f[ffffff]}" = "\x1b[38;2;255;255;255m",
+        "{#b[ffffff]}" = "\x1b[48;2;255;255;255m"
     );
 }
